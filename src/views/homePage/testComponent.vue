6702dd6689e4264ea83b7710536bfa5927d83d15
skillnull <skillisnull@gmail.com>
2019-07-29 21:03:27 +0800

    update

--- src/views/homePage/testComponent.vue
+++ src/views/homePage/testComponent.vue
@@ -1,6 +1,5 @@
 <template>
     <div style="text-align: left;padding: 20px;">
-        传递组件 <br>
         content = 'views/homePage/testComponent.vue' <br>
         contentType = 'component'
     </div>
